09fec739df951ef177fab223140557a57e52afcf
Name <yauheni.varapayeu@innowise.com>
2024-12-14 16:43:40 +0100

    Remove unnecessary files and dependencies, improve API methods logic and structure, clear test logic

--- pages/bookstorePages/profilePage.js
+++ pages/bookstorePages/profilePage.js
@@ -7,17 +7,18 @@ class ProfilePage extends BasePage {
 
     profilePageElements() {
         return {
-            tableTitleRow : "span.mr-2 a",
-            dropdownMenu: '[aria-label="rows per page"]',
+            tableTitleRow : this.getElementBySelector("span.mr-2 a").all(),
+            dropdownMenu: this.getElementBySelector('[aria-label="rows per page"]'),
         }
     }
     async getBooksTitlesInProfile(){
-        await this.page.waitForSelector(this.profilePageElements().tableTitleRow);
-        await this.getElementBySelector(this.profilePageElements().dropdownMenu).selectOption("10 rows");
-        const titlesRows = this.profilePageElements().tableTitleRow;
-        const addedBooksTitles = await this.page.$$eval(titlesRows, rows =>
-            rows.map(row => row.textContent)
-          );
+        await this.profilePageElements().dropdownMenu.selectOption("10 rows");
+        const titlesRows = await this.profilePageElements().tableTitleRow;
+        const addedBooksTitles = [];
+        for (const link of titlesRows) {
+            const title = await link.textContent();
+            addedBooksTitles.push(title);
+        }
           return addedBooksTitles;
     }
 }
