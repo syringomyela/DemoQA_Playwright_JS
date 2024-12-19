804c04827950eea8fdab5a0f8698166480a8b2f7
Name <yauheni.varapayeu@innowise.com>
2024-12-19 15:35:22 +0100

    Another try with API endpoinds

--- pages/bookstorePages/methodsAPI.js
+++ pages/bookstorePages/methodsAPI.js
@@ -8,14 +8,14 @@ class baseAPIInteraction {
     this.page = page;
     this.request = request;
     this.endpoint = bookstoreEndpoints;
-    this.baseURL = process.env.BASE_URL || 'https://demoqa.com';
+    this.baseURL = 'https://demoqa.com';
     this.userCredentials = userAuthentCredsBody;
   }
 
   async registerUserRequest() {
     const data = this.userCredentials;
     console.log(`${this.baseURL}${this.endpoint.account.user}`);
-    const registerResponse = await this.request.post(this.endpoint.account.user, data);
+    const registerResponse = await this.request.post(`${this.baseURL}${this.endpoint.account.user}`, data);
     return registerResponse;
   }
 
